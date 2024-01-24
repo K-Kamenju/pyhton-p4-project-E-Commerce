656cac1e3bfc7bcaf8799a45069138c2b88e23a1
Caleb <kamenjuhkiiru@gmail.com>
2024-01-24 12:45:29 +0300

    K - Navbar Complete

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../../Assets/logo.png'
-import cart from '../../Assets/cart.png'
 
 function Navbar() {
+
+    const [menu, setMenu] = useState('home')
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
@@ -12,14 +14,31 @@ function Navbar() {
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav ms-auto">
-                    <a className="nav-link" aria-current="page" href="#">Home</a>
-                    <a className="nav-link" href="#">About</a>
-                    <a className="nav-link" href="#">Products</a>
+                <div className="navbar-nav ms-auto link-color">
+                    <a 
+                    className="nav-link " 
+                    aria-current="page" 
+                    href="#" 
+                    onClick={()=>{setMenu("home")}}>
+                        Home{menu==="home"?<hr/>:<></>}
+                    </a>
+                    <a 
+                    className="nav-link" 
+                    href="#" 
+                    onClick={()=>{setMenu("about")}}>
+                        About{menu==="about"?<hr/>:<></>}
+                    </a>
+                    <a 
+                    className="nav-link" 
+                    href="#"
+                    onClick={()=>{setMenu("products")}}>
+                        Products{menu==="products"?<hr/>:<></>}
+                    </a>
                 </div>
                 <div className="navbar-nav ms-auto">
-                    <button className="btn btn-sm btn-outline-secondary p-2">Login</button>
-                    <img src={cart} alt="cart" className="navbar-brand cart" />
+                    <button className="btn btn-sm btn-outline-success p-2 m-2">Login</button>
+                    <i className="fa fa-shopping-cart fa-3x" aria-hidden="true"></i>
+                    <div className="nav-cart-count">0</div>
                 </div>
             </div>
         </div>
