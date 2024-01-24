85489cb6206a160f5316a192511dd9c9e282c2ac
Caleb <kamenjuhkiiru@gmail.com>
2024-01-24 11:07:23 +0300

    K - Added components folder

---
+++ client/src/Components/App/App.js
@@ -0,0 +1,14 @@
+
+import Navbar from '../Navbar/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+       <h1>Hello World</h1>
+       <Navbar />
+    </div>
+  );
+}
+
+export default App;
